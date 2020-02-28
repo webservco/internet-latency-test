1e2e5a103703c0afb4b5d53dce960193ea4950f7
Radu Maier <ariademur@webserv.co>
2020-02-28 10:46:49 +0100

    rewrite program

--- internet-latency-test.sh
+++ internet-latency-test.sh
@@ -3,7 +3,7 @@
 # webservco/internet-latency-test
 # Test internet latency using the ping command and write the results in a CSV file.
 
-P_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/" # program path
+p_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/" # program path
 
 # Check external command
 command -v ping >/dev/null 2>&1 || {
@@ -12,47 +12,41 @@ command -v ping >/dev/null 2>&1 || {
 }
 
 # Check if configuration file is present
-if [ ! -f "${P_PATH}config.sh" ]; then
+if [ ! -f "${p_path}config.sh" ]; then
     echo "Configuration file is missing"
     return 1
 fi
 
-. "${P_PATH}config.sh" # load custom configuration
+. "${p_path}config.sh" # load custom configuration
 
-LOG_DATE=$(date '+%Y-%m-%d') # log file date format
-LOG_FILE="${LOG_PATH}${LOG_NAME}-$LOG_DATE.csv" # log file name
+log_date=$(date '+%Y-%m-%d') # log file date format
+log_file="${log_path}${log_name}-$log_date.csv" # log file name
 
-mkdir -p $(dirname $LOG_FILE) # create log dir if not exists
+mkdir -p $(dirname $log_file) # create log dir if not exists
 
-# Default data
-r_transmitted=0
-r_received=0
-r_loss=0
-r_time=0
-r_min=0
-r_avg=0
-r_max=0
-r_mdev=0
-
-RESULT=$(LC_ALL=C ping $PING_HOST -w $PING_TIME)
+result=$(LC_ALL=C ping $ping_host -w $ping_time)
 
 # Parse result
 
-r_transmitted=$(echo "$RESULT" | grep 'transmitted' | awk '{print $1}')
-r_received=$(echo "$RESULT" | grep 'transmitted' | awk '{print $4}')
-r_loss=$(echo "$RESULT" | grep 'transmitted' | awk '{print $6}' | sed 's/%//')
-r_time=$(echo "$RESULT" | grep 'transmitted' | awk '{print $10}' | sed 's/ms//')
-r_min=$(echo "$RESULT" | grep 'rtt ' | awk '{print $4}' | awk -F "/" '{print $1}')
-r_avg=$(echo "$RESULT" | grep 'rtt ' | awk '{print $4}' | awk -F "/" '{print $2}')
-r_max=$(echo "$RESULT" | grep 'rtt ' | awk '{print $4}' | awk -F "/" '{print $3}')
-r_mdev=$(echo "$RESULT" | grep 'rtt ' | awk '{print $4}' | awk -F "/" '{print $4}')
+r_message=''
+r_transmitted=$( echo "$result" | grep -oP '\d+(?= packets transmitted)' )
+[[ -z "$r_transmitted" ]] && { r_message=$(echo "$result"); }
+r_received=$( echo "$result" | grep -oP '\d+(?= received)' )
+r_errors=$( echo "$result" | grep -oP '\d+(?= errors)' )
+[[ -z "$r_errors" ]] && { r_errors=0; }
+r_loss=$( echo "$result" | grep -oP '\d+(?=% packet loss)' )
+r_time=$( echo "$result" | grep -oP ".*time \K\d+" )
+r_min=$(echo "$result" | grep 'rtt ' | awk '{print $4}' | awk -F "/" '{print $1}')
+r_avg=$(echo "$result" | grep 'rtt ' | awk '{print $4}' | awk -F "/" '{print $2}')
+r_max=$(echo "$result" | grep 'rtt ' | awk '{print $4}' | awk -F "/" '{print $3}')
+r_mdev=$(echo "$result" | grep 'rtt ' | awk '{print $4}' | awk -F "/" '{print $4}')
 
 # If log file doesn't exist yet, write the header line
-if [ ! -f $LOG_FILE ]; then
-    echo "Date,Transmitted,Received,Loss,Time,Min,Avg,Max,Mdev" >> $LOG_FILE
+if [ ! -f $log_file ]; then
+    echo "Date,Transmitted,Received,Errors,Loss,Time,Min,Avg,Max,Mdev,Message" >> $log_file
 fi
 
-LOG_TIME=$(date '+%Y-%m-%dT%H:%M:%S') # log time format
+log_time=$(date '+%Y-%m-%dT%H:%M:%S') # log time format
 
 #  Write result in log file
-echo "$LOG_TIME,$r_transmitted,$r_received,$r_loss,$r_time,$r_min,$r_avg,$r_max,$r_mdev" >> $LOG_FILE
+echo "$log_time,$r_transmitted,$r_received,$r_errors,$r_loss,$r_time,$r_min,$r_avg,$r_max,$r_mdev,$r_message" >> $log_file
