cc8dc3e979856a5303ab43a8cf232ca11a0573ed
Radu Maier <ariademur@webserv.co>
2020-02-29 11:01:16 +0100

    Handle errors, add debug flag

--- internet-latency-test.sh
+++ internet-latency-test.sh
@@ -24,22 +24,25 @@ log_file="${log_path}${log_name}-$log_date.csv" # log file name
 
 mkdir -p $(dirname $log_file) # create log dir if not exists
 
-result=$(LC_ALL=C ping $ping_host -w $ping_time)
+result=$( LC_ALL=C ping $ping_host -w $ping_time 2>&1 )
 
 # Parse result
 
-r_message=''
 r_transmitted=$( echo "$result" | grep -oP '\d+(?= packets transmitted)' )
-[[ -z "$r_transmitted" ]] && { r_message=$(echo "$result"); }
 r_received=$( echo "$result" | grep -oP '\d+(?= received)' )
 r_errors=$( echo "$result" | grep -oP '\d+(?= errors)' )
-[[ -z "$r_errors" ]] && { r_errors=0; }
+#[[ -z "$r_errors" ]] && { r_errors=0; }
 r_loss=$( echo "$result" | grep -oP '\d+(?=% packet loss)' )
 r_time=$( echo "$result" | grep -oP ".*time \K\d+" )
 r_min=$(echo "$result" | grep 'rtt ' | awk '{print $4}' | awk -F "/" '{print $1}')
 r_avg=$(echo "$result" | grep 'rtt ' | awk '{print $4}' | awk -F "/" '{print $2}')
 r_max=$(echo "$result" | grep 'rtt ' | awk '{print $4}' | awk -F "/" '{print $3}')
 r_mdev=$(echo "$result" | grep 'rtt ' | awk '{print $4}' | awk -F "/" '{print $4}')
+
+r_message=''
+if [ "$debug" = 1 ]; then
+    r_message=$(echo "$result")
+fi
 
 # If log file doesn't exist yet, write the header line
 if [ ! -f $log_file ]; then
